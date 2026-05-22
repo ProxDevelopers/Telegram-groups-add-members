e963d3861466d71bb4b6725748f0a520be730d18
Pro xDevelopers <163398127+ProxDevelopers@users.noreply.github.com>
2026-05-22 19:38:35 +0530

    Add files via upload

---
+++ tg-group-member-add.py
@@ -0,0 +1,24 @@
+import configparser
+import sys
+import csv
+import colorama
+import subprocess, requests, time, os
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+bluec = Style.BRIGHT + Fore.BLUE
+redc = Style.BRIGHT + Fore.RED
+greenc = Style.BRIGHT + Fore.GREEN
+whitec = Style.BRIGHT + Fore.WHITE
+magentac = Style.BRIGHT + Fore.MAGENTA
+cyanc = Style.BRIGHT + Fore.CYAN
+
+
+print("")
+print(f"{magentac}[*] Telegram Member add ")
+print()
+print(f"{greenc}[*] www.KenzaByte.com")
+print()
+print(f"{cyanc}[*]https://www.kenzabyte.com/telegram-members-adders/")
+print()
+a=input("[*] Press any key to exit...")
